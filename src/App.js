11a11ef6edc64b7cc60fc5fee6be1c33d1e86189
Elliot Juniper <elliotjuniper@gmail.com>
2022-04-25 13:41:43 +0100

    1

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import { BiShoppingBag } from "react-icons/bi";
+import './App.css';
+import Navbar from "./Navbar"
+import Data from './Data'
+import React from "react";
+import Modal from './Modal'
+
+
+
+
+
+const App = () => {
+
+  const [basket, setBasket] = useState([]);
+  const [show, setShow] = useState(false)
+
+    const handleAddProduct = (i) => {
+      setBasket([...basket, i])
+    }
+    console.log(basket)
+    
+    const handleRemoveProduct = (i) => {
+      const removeBasket = basket.filter(cat => cat.id !== i.id)
+        setBasket(removeBasket)
+    }
+
+  return (
+
+    <div>
+
+        <div className='App'>
+        <div className='basketButton'>
+          <BiShoppingBag className="checkout" onClick={() => setShow(true)}></BiShoppingBag>
+          </div>
+          <Modal 
+          basket={basket}
+          handleRemoveProduct={handleRemoveProduct}
+          onClose={() => setShow(false)} show={show}/>
+
+        </div>
+
+        <Navbar />
+        <Data 
+        handleAddProduct={handleAddProduct}
+        handleRemoveProduct={handleRemoveProduct}
+        />
+
+    </div>
+
+  )
+}
+      
+export default App
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+// import { Link } from "react-router-dom";
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navBar'>
+            <h1>Rent a Cat</h1>
+            {/* <Link to="/basket">Basket</Link> */}
+        </div>
+    )
+}
+
+export default Navbar
